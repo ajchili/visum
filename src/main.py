6f13708b2c9299b95625fa77fb6aa3aa5848f361
Kirin Patel <kirinpatel@gmail.com>
2024-12-29 12:08:52 -0500

    🔧 update root page to allow user video input

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from .routers import summary
 
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
 app.include_router(summary.router)
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="root.html", context={})
